9076100eee429ebc65fe02a8f696f1a4b87700e1
Zachary <zvcoding@gmail.com>
2021-09-27 17:36:31 -0400

    Completed ZV

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-// Write your code here!
-
 main.remove();
-
-const newHeader = document.createElement("h1");
-document.body.appendChild(newHeader);
-
-newHeader.id = "victory";
-
-newHeader.innerHTML = "YOUR-NAME is the champion";
+let newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+let newBody = document.querySelector('body');
+newBody.innerHTML = newHeader;
+let yourName = 'Lisa';
+newHeader.textContent = `${yourName} is the champion`;
